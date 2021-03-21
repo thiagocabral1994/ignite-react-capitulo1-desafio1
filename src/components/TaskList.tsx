e91862fc501cd821b1dacb49d5b3b3bf86ba5915
Thiago Cabral <thiago.cabral@ice.ufjf.br>
2021-03-21 11:54:01 -0300

    Função handleCreateNewTask

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,7 +15,20 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (!newTaskTitle) {
+      alert('Escreve um todo!');
+      return;
+    }
+
+    const newId: number = new Date().valueOf();
+    
+    const newTask: Task = {
+      id: newId,
+      title: newTaskTitle,
+      isComplete: false
+    };
+
+    setTasks([...tasks, newTask]);
   }
 
   function handleToggleTaskCompletion(id: number) {
